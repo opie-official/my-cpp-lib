9dda184e2d5d02f54528c76b8257140099d56da1
OPIE <blackwatch7658@mail.ru>
2026-01-14 13:19:37 +0300

    completed Array

--- opie/collections/array.h
+++ opie/collections/array.h
@@ -30,7 +30,7 @@ namespace opie {
 
         ~Array() override= default;
 
-        Array &operator=(std::initializer_list<T>&cb);
+        Array &operator=(std::initializer_list<T>&init);
         Array &operator=(const T (&other)[N]);
         Array&operator=(const Array&other);
         Array&operator=(Array&&other)noexcept;
@@ -45,7 +45,112 @@ namespace opie {
 
         Iterator<T> end() const override;
 
+
     };
+
+    template<typename T, size_t N>
+    Array<T, N>::Array(const std::initializer_list<T> &init) {
+        if (N<=init.size()) {
+            auto ptr = init.begin();
+            for (size_t i=0;i<N;i++) {
+                _data[i]=*(ptr+i);
+            }
+        }else {
+            auto ptr = init.begin();
+            for (size_t i=0;i<init.size();i++) {
+                _data[i]=*(ptr+i);
+            }
+        }
+    }
+
+    template<typename T, size_t N>
+    Array<T, N>::Array(const T(&other)[N]) {
+        for (size_t i=0;i<N;i++) {
+            _data[i]=other[i];
+        }
+    }
+
+    template<typename T, size_t N>
+    Array<T, N>::Array(const Array &other) {
+        for (size_t i=0;i<N;i++) {
+            _data[i]=other._data[i];
+        }
+    }
+
+    template<typename T, size_t N>
+    Array<T, N>::Array(Array &&other) noexcept {
+        for (size_t i=0;i<N;i++) {
+            _data[i]=std::move(other._data[i]);
+        }
+    }
+
+    template<typename T, size_t N>
+    Array<T, N> & Array<T, N>::operator=(std::initializer_list<T> &init) {
+        if (N<=init.size()) {
+            auto ptr = init.begin();
+            for (size_t i=0;i<N;i++) {
+                _data[i]=*(ptr+i);
+            }
+        }else {
+            auto ptr = init.begin();
+            for (size_t i=0;i<init.size();i++) {
+                _data[i]=*(ptr+i);
+            }
+        }
+        return *this;
+    }
+
+    template<typename T, size_t N>
+    Array<T, N> & Array<T, N>::operator=(const T(&other)[N]) {
+        for (size_t i=0;i<N;i++) {
+            _data[i]=other[i];
+        }
+        return *this;
+    }
+
+    template<typename T, size_t N>
+    Array<T, N> & Array<T, N>::operator=(const Array &other) {
+        for (size_t i=0;i<N;i++) {
+            _data[i]=other._data[i];
+        }
+        return*this;
+    }
+
+    template<typename T, size_t N>
+    Array<T, N> & Array<T, N>::operator=(Array &&other) noexcept {
+        for (size_t i=0;i<N;i++) {
+            _data[i]=std::move(other._data[i]);
+        }
+        return*this;
+    }
+
+    template<typename T, size_t N>
+    Option<T> Array<T, N>::operator[](size_t i) {
+        if (i<N) {
+            return _data[i];
+        }
+        return none;
+    }
+
+    template<typename T, size_t N>
+    T * Array<T, N>::data() const {
+        return _data;
+    }
+
+    template<typename T, size_t N>
+    size_t Array<T, N>::length() const {
+        return N;
+    }
+
+    template<typename T, size_t N>
+    Iterator<T> Array<T, N>::begin() const {
+        return Iterator<T>{_data, N+1, 0};
+    }
+
+    template<typename T, size_t N>
+    Iterator<T> Array<T, N>::end() const {
+        return Iterator<T>{_data, N+1, N};
+    }
 }
 
 #endif //OPIE_DECLARATIVE_LIB_ARRAY_H
